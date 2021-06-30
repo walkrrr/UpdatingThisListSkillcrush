88a0e49bd03c53a55592a3680aa039069724550a
RWR <62685147+walkrrr@users.noreply.github.com>
2021-06-30 04:16:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+line_sisters=["Julie", "Viv", "Leticia", "Reni", "Meli", "Angelique", "Liz", "Kelly", "Michele", "Ebony", "Angie", "Sarah", "Quia", "Faye", "Jennifer", "Christine", "Patricia", "Denise", "Carolina", "DJ"]
+
+print(line_sisters[0:4])
+
+print(line_sisters[16:20])
+
+print(line_sisters[10:14])
+
+print(line_sisters[8])
